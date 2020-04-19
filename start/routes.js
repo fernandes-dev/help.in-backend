f9fba1209ae512d9cf12b5fc0042f3e373d03a6e
fernandes-dev <eduardo.yugan@gmail.com>
2020-04-19 05:13:06 -0400

    added routes

--- start/routes.js
+++ start/routes.js
@@ -1,19 +1,12 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
 Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' };
+  return { msg: 'Seja Bem Vindo' };
 });
+
+Route.resource('adress', 'AdressController').apiOnly();
+Route.resource('user', 'UserController').apiOnly();
+Route.post('session', 'UserSessionController.create');
+Route.resource('company', 'CompanyController').apiOnly();
+Route.resource('company-category', 'CompanyCategoryController').apiOnly();
